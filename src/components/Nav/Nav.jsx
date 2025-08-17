653686db3ebf90d2b979d4d127765972ccb522b8
Mohammed Ali Ahmed Mohammed Yousif Ahmed Din <mohd73480@gmail.com>
2025-08-17 15:32:45 +0300

    Nav code

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,12 +1,20 @@
+import { NavLink } from "react-router-dom";
 
-import { Link } from "react-router-dom";
-export default function Nav(){
+const linkClass = ({ isActive }) =>
+  "nav-link" + (isActive ? " nav-link--active" : "");
+
+export default function Nav() {
   return (
-    <nav className="nav">
-      <Link to="/"><div>HOME</div></Link>
-      <Link to="/about"><div>ABOUT</div></Link>
-      <Link to="/search"><div>SEARCH</div></Link>
-      <Link to="/movie/:id"><div>MOVIE</div></Link>
+    <nav className="nav" aria-label="Main navigation">
+      <NavLink to="/" end className={linkClass}>
+        Home
+      </NavLink>
+      <NavLink to="/about" className={linkClass}>
+        About
+      </NavLink>
+      <NavLink to="/search" className={linkClass}>
+        Search
+      </NavLink>
     </nav>
   );
 }
